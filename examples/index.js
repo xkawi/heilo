09e737675ce5b258d5578c95bf0e5a156a47613d
Kawi Xiao <kawi.xiao@qanvast.com>
2016-12-20 16:40:11 +0800

    update example

--- examples/index.js
+++ examples/index.js
@@ -2,7 +2,7 @@
 
 const heilo = require('../lib')
 
-// heilo('http://localhost:8000', { interval: '0.1m' })
-heilo('http://localhost:8000', {
-  interval: '0.1m'
+heilo('http://example.com', {
+  interval: '5m',
+  debug: true
 })
