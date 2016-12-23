0e6f62b476369a9abc02c43e35a0db45787c7c69
Kawi Xiao <kawixiao@gmail.com>
2016-12-23 23:03:12 +0800

    Fix eslint errors

--- examples/heilo-mail/index.js
+++ examples/heilo-mail/index.js
@@ -1,5 +1,5 @@
-const nodemailer = require("nodemailer")
-const heilo = require("heilo")
+const nodemailer = require('nodemailer')
+const heilo = require('heilo')
 
 const mailTransport = nodemailer.createTransport({ direct: true, debug: true })
 
@@ -9,18 +9,13 @@ const to = process.env.TO_EMAIL || 'bbb'
 const hostname = process.env.HOSTNAME || 'https://localhost'
 
 const reportError = (error) => {
-  const {
-    hostname, statusCode, message,
-    type, errorType, originalError,
-    timestamp
-  } = error;
   console.log('Server Down, sending notification...')
   mailTransport.sendMail({
     from,
     to,
     subject: 'Server Down!',
-    text: `${hostname} is down with status code ${statusCode} and error message - ${message}`,
-    html: `<p>${hostname} is down with status code ${statusCode} and error message - ${message}</p>`
+    text: `${error.hostname} is down with status code ${error.statusCode} and error message - ${error.message}`,
+    html: `<p>${error.hostname} is down with status code ${error.statusCode} and error message - ${error.message}</p>`
   })
 }
 
